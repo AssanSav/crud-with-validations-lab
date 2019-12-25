b105d9a7075b27647eababf58226338e65d34876
AssanSav <assane.savadogo81@gmail.com>
2019-12-25 01:19:20 -0500

    Done.

---
+++ app/models/song.rb
@@ -0,0 +1,12 @@
+class Song < ApplicationRecord
+    validates :title, presence: true
+    validates :released, inclusion: {in: [true, false]}
+    validates :release_year, inclusion: {in: 1..Time.current.year}, if: :released?
+    validate :not_twice_in_the_same_year?
+
+    def not_twice_in_the_same_year?
+        if Song.any? {|a| a.title == title && a.artist_name == artist_name && a.release_year == release_year}
+            errors.add(:release_year, "Forbidden to release the same song title twice within a year!")
+        end
+    end
+end
